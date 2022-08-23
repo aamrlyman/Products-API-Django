3951964c0194802552d2e536037135bbbf9c3f1a
aamrlyman <aamrlyman@gmail.com>
2022-08-22 20:35:02 -0700

    create superuser, add CRUD functions

--- products/views.py
+++ products/views.py
@@ -1,8 +1,37 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from .models import Product
+from .serializers import ProductSerializer
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def products_list(request):
 
-    return Response('Success')
+    if request.method == 'GET':
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404 (Product, pk=pk)
+    if request.method == "GET":
+        serializer = ProductSerializer(Product);
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(Product, data = request.data);
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data) 
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+        
