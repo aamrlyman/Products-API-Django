5091409caf47844c436c4493cff77f6efa0857ee
aamrlyman <aamrlyman@gmail.com>
2022-08-23 15:10:15 -0700

    fix product_detail function, add delete function to views file, add postman collection to git

--- products/views.py
+++ products/views.py
@@ -1,4 +1,3 @@
-from distutils.command.sdist import sdist
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -32,17 +31,19 @@ def products_list(request):
 #         serializer.is_valid(raise_exception=True)
 #         serializer.save()
 #         return Response(serializer.data) 
-#     elif request.method == "DELETE":
-#         product.delete()
-#         return Response(status = status.HTTP_204_NO_CONTENT)
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 def product_detail(request, pk):
-    try:
-        product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product);
         return Response(serializer.data)
-
-    except Product.DoesNotExist:
-        return Response(status= status.HTTP_404_NOT_FOUND);
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data);
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+             product.delete()
+             return Response(status = status.HTTP_204_NO_CONTENT)
 
